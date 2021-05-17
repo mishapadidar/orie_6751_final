50a08683641eda3d601d2c60b670e39bce6a07ae
Misha Padidar <map454@cornell.edu>
2021-05-16 22:17:04 -0400

    setup first problem

--- experiments/test.py
+++ experiments/test.py
@@ -3,8 +3,10 @@ import pickle
 import sys
 sys.path.append('../optim/')
 from bfgs import BFGS
+from gradient_descent import gradient_descent
 sys.path.append('../utils/')
 from get_input_settings import get_input_settings
+from finite_difference import fdiff_jac
 sys.path.append('../problem/')
 from normal_covariances import compute_fourier_coefficient_covariance_for_FOCUS
 from perturb_coil import perturb_coil
@@ -29,7 +31,7 @@ batch_size = run_params['batch_size']
 max_iter = run_params['max_iter']
 mu0 = run_params['mu0']
 mu_min = run_params['mu_min']
-mu0_max = run_params['mu_max']
+mu_max = run_params['mu_max']
 gtol = run_params['gtol']
 c_armijo = run_params['c_armijo']
 alpha_pen = run_params['alpha_pen']
@@ -76,7 +78,7 @@ if problem_num == 0:
   s.t. curvature constraint
   """
   # append the CVaR t parameter
-  t0 = 1.0
+  t0 = 0.0
   x0 = np.append(x0,t0)
 
   # CVaR objective
@@ -84,51 +86,89 @@ if problem_num == 0:
     # split x and t
     t = y[-1]
     x = y[:-1]
-    # evaluate the constraints
-    f_pen = test.fvec(x,['curv'])
+    ## evaluate the constraints
+    f_pen = test.fvec(x,['curv'])[0]
     # reset the seed so we get the same perturbations
     np.random.seed(seed)
     _X = [perturb_coil(x,C,max_rotation,max_shift,n_coils,nfcoil) for ii in range(batch_size)]
+    #_X = np.array([x,x])
     # compute the CVaR
-    _fX = np.array([test.fvec(_x,['bnorm']) for _x in _X])
+    _fX = np.array([test.fvec(_x,['bnorm'])[0] for _x in _X])
     f_stoch = np.mean(np.maximum(_fX - t,0.0))/delta + t
     # make penalty obj
-    F = f_stoch + f_pen
+    F = f_stoch + alpha_pen*f_pen
+
+    # non-stochastic test
+    #fs = test.fvec(x,['bnorm','curv'])
+    #f_stoch = np.mean(np.maximum(fs[0] - t,0.0))/delta + t
+    #F = f_stoch + alpha_pen*fs[1]
     return F
 
   def Grad(y):
-    # split x and t
+    ## split x and t
     t = y[-1]
     x = y[:-1]
     # evaluate the constraint gradients
-    g_pen = test.gradvec(x,['curv']) 
+    g_pen = test.gvec(x,['curv'])[0]
     # reset the seed so we get the same perturbations
     np.random.seed(seed)
     _X = [perturb_coil(x,C,max_rotation,max_shift,n_coils,nfcoil) for ii in range(batch_size)]
+    #_X = np.array([x,x])
     # compute the CVaR gradient
-    _fX = np.array([test.fvec(_x,['bnorm']) for _x in _X])
-    _gX = np.array([test.gradvec(_x,['bnorm']) for _x in _X])
-    g_stoch = np.mean(np.diag(_fX-t > 0) @ _gX,axis=0)/delta
+    _fX = np.array([test.fvec(_x,['bnorm'])[0] for _x in _X])
+    _gX = np.array([test.gvec(_x,['bnorm'])[0] for _x in _X])
+    g_stoch = np.mean((_fX-t > 0.0) * _gX.T,axis=1)/delta
     # make penalty obj
-    dgdx = g_stoch + g_pen
+    dgdx = g_stoch + alpha_pen*g_pen
     # now get the t-derivatives
     dgdt = 1 - np.mean(_fX-t > 0)/delta
     # stack
     g = np.append(dgdx,dgdt)
+
+    # non-stochastic test
+    #fs = test.fvec(x,['bnorm','curv'])
+    #gs = test.gvec(x,['bnorm','curv'])
+    #g_stoch = (fs[0] - t > 0.0) * gs[0]/delta
+    #dgdx = g_stoch + alpha_pen*gs[1]
+    #dgdt = 1 - np.mean(fs[0]-t > 0)/delta
+    #g = np.append(dgdx,dgdt)
+
     return g
 
-print(Obj(x0))
-print(Grad(x0))
-quit()
+
+f0 = Obj(x0)
+#g0 = Grad(x0)
+#gfd = fdiff_jac(Obj,x0,h=1e-3)
+if verbose:
+  print(f"Starting from Objective value of {f0}")
+  #print(g0)
+  #print(gfd)
+  #print(g0-gfd)
+#quit()
+
 # optimize w/ BFGS
-xopt,X,fX = BFGS(Obj,Grad,x0,mu0 = mu0,max_iter=max_iter,
-  gtol=gtol,c_armijo=c_armijo,mu_min=mu_min,mu_max=mu_max)
+#def callback(x):
+#  f = Obj(x)
+#  if verbose:
+#    print(f)
+#    sys.stdout.flush()
+#from scipy.optimize import minimize
+#res = minimize(Obj, x0, method='BFGS', jac=Grad, callback=callback, options={'gtol':gtol,'maxiter':max_iter})
+#print(res)
+xopt,X,fX = gradient_descent(Obj,Grad,x0,mu0 = mu0,max_iter=max_iter,
+        gtol=gtol,c_armijo=c_armijo,mu_min=mu_min,mu_max=mu_max,verbose=verbose)
+#xopt,X,fX = BFGS(Obj,Grad,x0,mu0 = mu0,max_iter=max_iter,
+#  gtol=gtol,c_armijo=c_armijo,mu_min=mu_min,mu_max=mu_max,verbose=verbose)
 
 # get the function values along the trajectory
 fXopt          = fX[-1]
+gopt = Grad(xopt)
 if master:
   print(f"fopt: {fXopt}")
+  print(f"gopt: {gopt}")
+  print(f"norm(grad): {np.linalg.norm(gopt)}")
 
+# TODO: split the xopt from topt
 if master:
   # save the results
   d                    = {}
